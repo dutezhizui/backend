83a160b1a3569b104520fc4810804f38167289d6
yaojiaoyi <yaojiaoyi@dashuf.com>
2018-12-22 15:07:48 +0800

    feat:修改mybatis

--- src/main/java/com/darkcom/backend/controller/UserController.java
+++ src/main/java/com/darkcom/backend/controller/UserController.java
@@ -1,29 +1,9 @@
 package com.darkcom.backend.controller;
 
-import com.alibaba.fastjson.JSON;
-import com.darkcom.backend.common.Result;
-import com.darkcom.backend.common.enums.BizCodeEnum;
 import com.darkcom.backend.constants.Urls;
-import com.darkcom.backend.dto.request.UserRequest;
-import com.darkcom.backend.dto.response.UserResponse;
-import com.darkcom.backend.model.User;
-import com.darkcom.backend.service.UserService;
-import com.darkcom.backend.utils.AssertUtil;
 import io.swagger.annotations.Api;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.util.ObjectUtils;
-import org.springframework.web.bind.annotation.*;
-
-import javax.mail.MessagingException;
-import javax.mail.internet.MimeMessage;
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * 用户管理
@@ -34,72 +14,5 @@ import java.util.List;
 @RequestMapping(Urls.User.ROOT)
 @Api("用户相关")
 public class UserController {
-    public static final Logger logger = LoggerFactory.getLogger(UserController.class);
-    @Autowired
-    private UserService userService;
-    @Autowired
-    private JavaMailSender sender;
 
-    @PostMapping(Urls.User.USER)
-    public Result<UserResponse> addUser(@RequestBody UserRequest userRequest) {
-        logger.info("[addUser],userRequest={}", JSON.toJSONString(userRequest));
-        userService.addUser(userRequest);
-        return Result.succeed();
-    }
-
-    @GetMapping(Urls.User.USER_INFO)
-    public Result<UserResponse> getUserInfo(@RequestParam("account") String account) {
-        User user = userService.getUserByAccount(account);
-        UserResponse userResponse = new UserResponse();
-        AssertUtil.isTrueForRpc(!ObjectUtils.isEmpty(user), BizCodeEnum.DATA_NOT_FOUND);
-        BeanUtils.copyProperties(user, userResponse);
-        return new Result<>(userResponse);
-    }
-
-    @PostMapping("/send")
-    public void sendmail() {
-        MimeMessage message = sender.createMimeMessage();
-        //true表示需要创建一个multipart message
-        MimeMessageHelper helper = null;
-        User user1 = new User();
-        user1.setAccount("yaojiaoyi");
-        user1.setPhone("15989510265");
-        User user2 = new User();
-        user2.setAccount("yaojiaoyi");
-        user2.setPhone("15989510265");
-        List<User> list = new ArrayList();
-        list.add(user1);
-        list.add(user2);
-        String html = "";
-        for (int i = 0; i < list.size(); i++) {
-
-            html += "  <tr>\n" +
-                    "    <td>" + list.get(i).getAccount() + "</td>\n" +
-                    "    <td>" + list.get(i).getPhone() + "</td>\n" +
-                    "  </tr>\n";
-        }
-        String content = "<html>\n" +
-                "<body>\n" +
-                "    <h3>hello world ! 这是一封Html邮件!</h3>\n" +
-                "<p>这是一封测试邮件，一下这些组合超过限额</>"+
-                "<table border=\"1\">\n" +
-                "  <tr>\n" +
-                "    <th>Month</th>\n" +
-                "    <th>Savings</th>\n" +
-                html +
-                "</table>" +
-                "</body>\n" +
-                "</html>";
-        try {
-            helper = new MimeMessageHelper(message, true);
-            helper.setFrom("841646726@qq.com");
-            helper.setTo("841646726@qq.com");
-            helper.setSubject("html mail");
-            helper.setText(content, true);
-        } catch (MessagingException e) {
-            e.printStackTrace();
-        }
-
-        sender.send(message);
-    }
 }
--- src/main/java/com/darkcom/backend/model/User.java
+++ src/main/java/com/darkcom/backend/model/User.java
@@ -1,36 +1,30 @@
 package com.darkcom.backend.model;
 
-import org.springframework.data.jpa.domain.AbstractPersistable;
+public class User {
+    private Long id;
 
-import javax.persistence.*;
-
-@Entity
-@Table(name = "t_user")
-public class User extends AbstractPersistable<Long> {
-
-    @Column(unique = true, length = 20, nullable = false, columnDefinition = "varchar(20) comment '账号'")
     private String account;
-    @Column(length = 15, columnDefinition = "varchar(20) comment '手机号'")
-    private String phone;
-    @Column(length = 20, columnDefinition = "varchar(20) comment '姓名'")
+
     private String name;
-    @Column(length = 256, nullable = false, columnDefinition = "varchar(256) comment '密码'")
+
     private String password;
+
+    private String phone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getAccount() {
         return account;
     }
 
     public void setAccount(String account) {
-        this.account = account;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
+        this.account = account == null ? null : account.trim();
     }
 
     public String getName() {
@@ -38,7 +32,7 @@ public class User extends AbstractPersistable<Long> {
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.name = name == null ? null : name.trim();
     }
 
     public String getPassword() {
@@ -46,6 +40,14 @@ public class User extends AbstractPersistable<Long> {
     }
 
     public void setPassword(String password) {
-        this.password = password;
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
     }
 }
